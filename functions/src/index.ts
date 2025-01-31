545d0fdb37a1ffa5d4a824adc831859954e02f57
Andrew Nguyen <anguy424@ucsc.edu>
2025-01-30 23:38:47 -0800

    Wrote fetchPrompt cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,6 +12,28 @@ export const fetchQuestion = functions.https.onRequest(async (req, res) => {
       if (qPool.length == 0) {
         console.log("Question prompt database is empty");
       }
+
+      // Get random document from database
+      const randomIndex = Math.floor(Math.random() * qPool.length);
+      const data = qPool[randomIndex].data();
+      res.send(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching question", error);
+      res.status(500).send({error: "Internal Server Error"});
+    });
+});
+
+export const fetchPrompt = functions.https.onRequest(async (req, res) => {
+  // Inefficient if our question database is big
+  admin.firestore().collection("random-prompts").get()
+    .then((snapshot) => {
+      const qPool = snapshot.docs;
+      if (qPool.length == 0) {
+        console.log("Random prompt database is empty");
+      }
+
+      // Get random document from database
       const randomIndex = Math.floor(Math.random() * qPool.length);
       const data = qPool[randomIndex].data();
       res.send(data);
