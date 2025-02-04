2be966516195fa156c19de4aba2aef2d7c3601d7
Peter Lee <peterhlee2004@gmail.com>
2025-02-03 19:19:26 -0800

    Setup skeleton for the journal entry page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,8 @@ export default function SetupLayout() {
   return (
     <Stack>
         <Stack.Screen name="(home)" options={{ headerShown: false }}/>
-        <Stack.Screen name="(add-journal)" options={{ headerShown: false}}/>
+        <Stack.Screen name="(add-journal)" options={{ headerShown: false }}/>
+        <Stack.Screen name="(entries)" options={{ headerShown: false }} />
     </Stack>
   );
 }
