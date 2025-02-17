08ee485a8837d1be736f21370590a67aef114144
Matthew Kimotsuki <matthewkimotsuki@Matthews-MacBook-Air-2.local>
2025-02-16 18:39:41 -0800

    fixed headers + styling

--- app/(tabs)/(entries)/entries.tsx
+++ app/(tabs)/(entries)/entries.tsx
@@ -329,6 +329,7 @@ const styles = StyleSheet.create({
     paddingBottom: 100,
   },
   header: {
+    marginTop: 20,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
@@ -403,8 +404,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     paddingVertical: 20,
     backgroundColor: '#F0ECE0',
-    borderTopWidth: 1,
-    borderTopColor: '#70664533',
   },
   footerImage: {
     width: 50,
