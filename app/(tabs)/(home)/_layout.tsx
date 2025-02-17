f6c6e6cf16295b5fa2f0715de913e4851cd85e09
Matthew Kimotsuki <matthewkimotsuki@Matthews-MacBook-Air-2.local>
2025-02-16 18:39:41 -0800

    fixed headers + styling

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router"
 export default function SetupLayout() {
   return (
     <Stack>
-        <Stack.Screen name="homepage" options={{ headerShown: false }}/>
+        <Stack.Screen name="homepage" options={{ headerShown: false}}/>
         <Stack.Screen name="weekCalendar"/>
     </Stack>
   );
